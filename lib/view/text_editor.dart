04bb7cb090e09d65c546e2212dec69e59c592194
Murat Iyigun <miyigun@hotmail.com>
2023-11-13 22:44:42 +0300

    Commit 2

--- lib/view/text_editor.dart
+++ lib/view/text_editor.dart
@@ -6,10 +6,167 @@ class TextEditor extends ConsumerWidget{
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Merhaba',
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width*0.8,
+                height: 100,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/wood_floor.jpg"),
+                    fit: BoxFit.cover,
+                  ), //DecorationImage
+                  border: Border.all(
+                    //color: Colors.green,
+                    width: 1,
+                  ), //Border.all
+                  borderRadius: BorderRadius.circular(15),
+                  /*boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(
+                        5.0,
+                        5.0,
+                      ), //Offset
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    ), //BoxShadow
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0,
+                    ), //BoxShadow
+                  ],*/
+                ),
+                child: Row(
+                  //mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width:10),
+                    Container(
+                        decoration: const ShapeDecoration(
+                          color: Colors.white,
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.format_bold_sharp,
+                          ),
+                          iconSize: 30,
+                          onPressed: () {},
+                        )),
+                    const SizedBox(width:10),
+                    Container(
+                        decoration: const ShapeDecoration(
+                          color: Colors.white,
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.format_italic_sharp,
+                          ),
+                          iconSize: 30,
+                          onPressed: () {},
+                        )),
+                    const SizedBox(width:10),
+                    Container(
+                        decoration: const ShapeDecoration(
+                          color: Colors.white,
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.format_underline_sharp,
+                          ),
+                          iconSize: 30,
+                          onPressed: () {},
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width*0.8,
+                height: MediaQuery.of(context).size.height*0.8,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage("assets/images/wood_floor.jpg"),
+                    fit: BoxFit.cover,
+                  ), //DecorationImage
+                  border: Border.all(
+                    //color: Colors.green,
+                    width: 1,
+                  ), //Border.all
+                  borderRadius: BorderRadius.circular(15),
+                  /*boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: Offset(
+                        5.0,
+                        5.0,
+                      ), //Offset
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    ), //BoxShadow
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0,
+                    ), //BoxShadow
+                  ],*/
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/wood_floor.jpg"),
+                        fit: BoxFit.cover,
+                      ), //DecorationImage
+                      /*border: Border.all(
+                        //color: Colors.green,
+                        width: 1,
+                      ), *///Border.all
+                      //borderRadius: BorderRadius.circular(15),
+                      /*boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black,
+                        offset: Offset(
+                          5.0,
+                          5.0,
+                        ), //Offset
+                        blurRadius: 10.0,
+                        spreadRadius: 2.0,
+                      ), //BoxShadow
+                      BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 0.0,
+                        spreadRadius: 0.0,
+                      ), //BoxShadow
+                    ],*/
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: InputBorder.none
+                      ),
+                    ),
+
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
